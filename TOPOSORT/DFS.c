89ee62e31e7b392e9d7f8a2ba18d348121f61fb1
skysys007 <gagan07pradhan@gmail.com>
2026-04-15 08:05:58 +0530

    Implement comparison counting in DFS and topoSort
    Added comparison count to track DFS calls.

--- TOPOSORT/DFS.c
+++ TOPOSORT/DFS.c
@@ -7,39 +7,58 @@ int stack[MAX];
 int top = -1;
 int n;
 
+int comparisons = 0; // count comparisons
+
 void push(int v){
     stack[++top] = v;
-
 }
 
 void DFS(int v){
     visited[v] = 1;
-    for (int i = 0; i < n; i++)
-    {
-        DFS(i);
-    }
-    push(v);
-}
 
-void topoSort(int v){
-    visited[v] = 1;
-    for (int i = 0; i < n; i++)
-    {
-        if (!visited[i])
-        {
+    for (int i = 0; i < n; i++){
+        comparisons++; // for checking condition
+        if (adj[v][i] == 1 && !visited[i]){
             DFS(i);
         }
     }
-    while (top!=-1)
-    {
+
+    push(v);
+}
+
+void topoSort(){
+    for (int i = 0; i < n; i++){
+        comparisons++;
+        if (!visited[i]){
+            DFS(i);
+        }
+    }
+
+    printf("Topological Order:\n");
+    while (top != -1){
         printf("%d\t", stack[top--]);
     }
-    
-    
-    
+
+    printf("\nComparisons: %d\n", comparisons);
 }
 
 int main() {
-    
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    // initialize visited
+    for (int i = 0; i < n; i++){
+        visited[i] = 0;
+    }
+
+    topoSort();
+
     return 0;
 }
